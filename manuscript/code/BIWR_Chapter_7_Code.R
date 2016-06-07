9e7d253acda904ca8130235fa333bd60653bdab3
Rmadillo <Rmadillo@users.noreply.github.com>
2016-06-06 18:24:32 -0700

    Add files via upload

---
+++ manuscript/code/BIWR_Chapter_7_Code.R
@@ -0,0 +1,352 @@
+#####################################################################
+# Business Intelligence with R
+# Dwight Barry
+# Chapter 7 Code: Dataviz
+#
+# 
+#
+######################################################################
+
+## Plotting multivariate distributions
+
+### Heatmaps
+
+require(ggplot2)
+require(dplyr)
+
+bike_share_grp = group_by(bike_share_daily, weekday, mnth)
+
+bike_share_mean = summarise(bike_share_grp, mean=mean(casual))
+
+ggplot(bike_share_mean, aes(weekday, mnth)) +
+  geom_tile(aes(fill = mean)) +
+  scale_fill_gradient(low = "white", high = "darkgreen") +
+  xlab("Day of the Week") +
+  ylab("Month") +
+  ggtitle("Mean Daily Casual-Use Bike Sharing") +
+  scale_y_discrete(limits = rev(levels(bike_share_mean$mnth))) +
+  theme_bw() +
+  theme(plot.title = element_text(vjust = 1))
+
+bike_share_grp = group_by(bike_share_daily, weekday, mnth)
+
+bike_share_sd = summarise(bike_share_grp, sd=sd(casual))
+
+ggplot(bike_share_sd, aes(weekday, mnth)) +
+  geom_tile(aes(fill = sd)) +
+  scale_fill_gradient(low = "white", high = "darkgreen") +
+  xlab("Day of the Week") +
+  ylab("Month") +
+  ggtitle("Standard Deviation of Daily Casual-Use Bike Sharing") +
+  scale_y_discrete(limits = rev(levels(bike_share_sd$mnth))) +
+  theme_bw() +
+  theme(plot.title = element_text(vjust = 1))
+
+
+### Creating calendar heatmaps
+
+require(RColorBrewer)
+
+source("http://blog.revolutionanalytics.com/downloads/calendarHeat.R")
+
+green_color_ramp = brewer.pal(9, "Greens")
+
+calendarHeat(bike_share_daily$dteday, bike_share_daily$casual, varname="Casual Daily Bike Use", color="green_color_ramp")
+
+
+### Parallel coordinates plots
+
+require(GGally)
+require(gdata)
+
+qb_url = "http://ciberconta.unizar.es/leccion/multivar/QUIEBRA.XLS"
+
+quiebra_download = read.xls(qb_url, sheet=1, header=TRUE, stringsAsFactors=FALSE)
+
+quiebra = quiebra_download[,2:12]
+
+ggparcoord(data=quiebra, columns=c(2:10), groupColumn=11, scale="globalminmax", alphaLines=0.5)
+
+ggparcoord(data=quiebra, columns=c(2:10), groupColumn=11, scale="globalminmax", boxplot=TRUE, alphaLines=0.3)
+
+
+### Peeking at multivariate data with `dplyr` and a bubblechart
+
+customer_survey = read.table("Data/customer_survey.csv", sep=",", header=T)
+
+customer_survey %>%
+  filter(customer_purchases >= 500) %>%
+  group_by(state) %>%
+  summarize(mean_purchases = mean(customer_purchases, na.rm=T),
+  sd_purchases = sd(customer_purchases, na.rm=T),
+  sum_est = sum(est)) %>%
+  mutate(cv_purchases = round(sd_purchases / mean_purchases, 2)) %>%
+  ggplot(aes(mean_purchases, sd_purchases, color=cv_purchases, size=sum_est)) +
+  geom_point() +
+  scale_size(range=c(2,6)) +
+  theme_bw()
+
+
+## Plotting a table
+
+require(reshape2)
+require(gridExtra)
+
+bike_share_grp = group_by(bike_share_daily, yr, mnth)
+
+bike_share_mean = summarise(bike_share_grp, mean=mean(casual))
+
+bike_share_mean$mean = round(bike_share_mean$mean, 0)
+
+bike_share_mean_wide = dcast(bike_share_mean, yr~mnth)
+
+bike_share_mean_wide = rename(bike_share_mean_wide, Year = yr)
+
+ggplot(bike_share_mean_wide, aes(Year, Jan)) +
+  annotation_custom(tableGrob(bike_share_mean_wide, show.rownames = FALSE)) +
+  ggtitle("Mean Daily Casual Bike Share Use, by Month (2011-2012)") +
+  theme_minimal() +
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(), 
+  panel.grid.minor = element_blank(), axis.ticks = element_blank(), 
+  axis.text = element_blank(), axis.title = element_blank())
+
+
+## Interactive dataviz
+
+### Basic interactive plots
+
+# devtools::install_github("hrbrmstr/taucharts")
+require(taucharts)
+require(dplyr)
+
+bike_share_daily$weather = as.factor(ifelse(bike_share_daily$weathersit == 1, "Clear", ifelse(bike_share_daily$weathersit == 2, "Cloudy/Rainy", "Stormy")))
+
+bike_share_count = bike_share_daily %>% 
+  group_by(weather) %>%
+  summarize(count=n())
+
+bike_share_count_season = bike_share_daily %>% 
+  group_by(weather, season) %>%
+  summarize(count=n())
+  
+tauchart(bike_share_count) %>% 
+  tau_bar("weather", "count") %>% 
+  tau_tooltip()
+
+tauchart(bike_share_count) %>% 
+  tau_point("weather", "count") %>% 
+  tau_tooltip()
+
+tauchart(bike_share_count_season) %>% 
+  tau_bar("count", "season", "weather", horizontal=T) %>% 
+  tau_legend() %>%
+  tau_tooltip()
+
+tauchart(bike_share_daily) %>%
+  tau_point("atemp", "casual") %>%
+  tau_tooltip()
+
+tauchart(bike_share_daily) %>% 
+  tau_point(c('weather', 'atemp'), c('season', 'casual')) %>%
+  tau_tooltip()
+
+
+### Scatterplot matrix
+
+require(pairsD3)
+
+pairsD3(bike_share_daily[,11:14], group = bike_share_daily[,3], opacity = 0.7, tooltip = paste("Season: ", bike_share_daily$season, "<br/> Casual use count: ", bike_share_daily$casual), col = c("#0571b0", "#92c5de", "#ca0020", "#f4a582"))
+
+
+### Motionchart: a moving bubblechart
+
+# load the googleVis package
+
+library(googleVis)
+
+# load the data (an excerpt of data from gapminder.org)
+
+rosling_data = read.csv("https://raw.githubusercontent.com/Rmadillo/UW_HCA/master/data/rosling_data.csv")
+
+# create the motion plot using larger plot size than default
+plot(gvisMotionChart(rosling_data, idvar = "Country", timevar = "Year", sizevar = "Population", options=list(width = 700, height = 600)))
+
+
+### Interactive tables with `DT`
+
+require(DT)
+
+datatable(rosling_data, filter = "top")
+
+
+## Making maps in R
+
+# http://cran.r-project.org/web/views/Spatial.html
+
+### Basic point maps
+
+require(maps)
+map("county", xlim=c(min(ozone$x-0.5),max(ozone$x+0.5)), ylim=range(ozone$y), col="gray80")
+
+map("state", xlim=c(min(ozone$x-0.5),max(ozone$x+0.5)), ylim=range(ozone$y), col="gray60", add=TRUE)
+
+box(col="gray50")
+
+text(ozone$x, ozone$y, ozone$median, cex=0.5)
+  
+map('world', xlim=c(-12,45), ylim=c(35,60), col="gray90", fill=T)
+
+box()
+
+
+### Chloropleth maps
+
+require(choroplethr)
+require(choroplethrMaps)
+
+data(df_pop_county)
+
+county_choropleth(df_pop_county)
+
+county_choropleth(df_pop_county, legend="County\nPopulation", buckets=1, zoom=c("arizona", "colorado", "new mexico", "utah"))
+
+require(zipcode)
+
+data(df_pop_zip)
+
+zip_map(df_pop_zip, legend="Zip Code\nPopulation", buckets=1, zoom=c("arizona", "colorado", "new mexico", "utah"))
+
+
+### Chloropleth mapping with the *American Community Survey*
+
+# http://www.census.gov/data/developers/about/terms-of-service.html
+  
+require(acs)
+
+api.key.install("YOUR KEY HERE")
+
+require(choroplethr)
+require(choroplethrMaps)
+
+choroplethr_acs(tableId="B19113", map="county", buckets=4, endyear=2012)
+
+# http://factfinder2.census.gov/faces/affhel/jsf/pages/metadata.xhtml?lang=en&type=survey&id=survey.en.ACS_ACS
+
+
+### Using Shapefiles
+
+download.file("http://epp.eurostat.ec.europa.eu/cache/GISCO/geodatafiles/CNTR_2014_03M_SH.zip", destfile="world2014.zip")
+# © EuroGeographics for the administrative boundaries
+
+unzip("world2014.zip")
+
+require(maptools)
+
+worldmap2014 = readShapeLines("CNTR_2014_03M_SH/Data/CNTR_BN_03M_2014.shp", proj4string = CRS("+proj=longlat +datum=ETRS89"))
+
+plot(worldmap2014, xlim=c(7,16), ylim=c(35,60), col="gray60")
+
+box(col="gray30")
+
+# http://www.eea.europa.eu/data-and-maps/data/airbase-the-european-air-quality-database-8
+
+eu_o3 = read.table("https://raw.githubusercontent.com/Rmadillo/business_intelligence_with_r/master/manuscript/code/eu_o3.csv", sep=",", header=T)
+
+plot(worldmap2014, xlim=c(2,12), ylim=c(35, 70), col="gray60")
+
+box(col="gray30")
+
+text(eu_o3$longitude, eu_o3$latitude, round(eu_o3$statistic_value,0), cex=0.5, col="gray20")
+
+require(RColorBrewer)
+
+o3_colors = brewer.pal(5, "OrRd")
+
+require(classInt)
+
+o3_breaks = classIntervals(sort(eu_o3$statistic_value), n=5, style="quantile")
+
+plot(worldmap2014, xlim=c(2,12), ylim=c(35, 70), col="gray60")
+
+box(col="gray30")
+
+points(eu_o3$longitude, eu_o3$latitude, pch=15, cex=0.5, col=o3_colors[findInterval(eu_o3$statistic_value, o3_breaks$brks, all.inside=TRUE)])
+
+legend("topleft", title=expression(O[3]~Levels~(ppb)), inset=0.007, cex=0.8, legend=leglabs(round(o3_breaks$brks),0), fill=o3_colors, bg="white", border="gray60")
+
+
+### Using `ggmap` for point data and heatmaps
+
+require(dplyr) 
+
+# SPD Police Reports
+spd = read.csv("https://data.seattle.gov/api/views/7ais-f98f/rows.csv", header=T, strip.white = T)
+
+# Filter to 2 years with full data
+spd = filter(spd, Year >= 2014 & Year < 2016)
+
+# Filter to bike thefts only
+bike_theft = filter(spd, Summarized.Offense.Description == "BIKE THEFT")
+
+## Set up base map
+seattle_map = ggmap(get_map('Seattle, Washington', zoom=11, source='google', maptype='terrain'), base_layer = ggplot(aes(x = Longitude, y = Latitude), data = bike_theft))
+
+# Point map of bike theft
+seattle_map +  
+  geom_point(alpha=0.5, size=0.5) +
+  scale_alpha(guide = FALSE) +
+  ggtitle('Seattle Bike Thefts (2014-2015)') +
+  xlab('') +
+  ylab('') +
+  theme(axis.ticks=element_blank(), axis.text=element_blank(),
+  legend.position='none')
+
+# Density map of bike theft by year
+seattle_map +  
+  stat_density2d(aes(fill=..level..), 
+  alpha=0.5,
+  bins = 10,
+  geom = 'polygon') +
+  scale_alpha(guide = FALSE) +
+  ggtitle('Seattle Bike Thefts (2014-2015)') +
+  xlab('') +
+  ylab('') +
+  facet_wrap(~Year) +
+  theme(axis.ticks=element_blank(), axis.text=element_blank(),
+  legend.position='none')
+
+
+### Interactive maps with `leaflet`
+
+require(leaflet)
+
+bike_theft_dec = filter(bike_theft, Year==2014 & Month==12)
+
+bike_theft_jan = filter(bike_theft, Year==2015 & Month==1)
+
+bike_theft_interactive = leaflet() %>% 
+  addTiles() %>% 
+  setView(-122.3397, 47.6144, zoom = 13) %>%
+  addCircleMarkers(data = bike_theft_dec, 
+    lat = ~ Latitude, 
+    lng = ~ Longitude, 
+    popup = bike_theft$Hundred.Block.Location, 
+    group="December 2014", 
+    fillOpacity = 0.25, 
+    color="red", 
+    radius=4) %>%
+  addCircleMarkers(data = bike_theft_jan, 
+    lat = ~ Latitude, 
+    lng = ~ Longitude, 
+    popup = bike_theft$Hundred.Block.Location, 
+    group="January 2015", 
+    fillOpacity = 0.25, 
+    color="blue", 
+    radius=4) %>%
+  addLayersControl(overlayGroups = c("December 2014", "January 2015"), 
+    options = layersControlOptions(collapsed = FALSE)) %>%
+    addLegend("bottomright",  title = "Legend", 
+    colors=c("red","blue"), labels=c("December 2014", "January 2015"))
+
+
+##### End of File #####
---
+++ manuscript/code/BIWR_Chpater_6_Code.R
@@ -0,0 +1,365 @@
+#####################################################################
+# Business Intelligence with R
+# Dwight Barry
+# Chapter 6 Code: Trends and Time
+#
+# 
+#
+######################################################################
+
+## Describing trends in non-temporal data
+  
+require(ggplot2)
+require(quantreg)
+
+### Smoothed trends
+
+ggplot(bike_share_daily, aes(x=atemp, y=cnt)) +
+  xlab("Daily Mean Normalized Air Temperature") +
+  ylab("Number of Total Bike Uses") +
+  geom_point(col="gray50") +
+  geom_smooth(method="loess") +
+  theme_bw()
+
+require(mgcv)
+
+ggplot(bike_share_daily, aes(x=atemp, y=cnt)) +
+  xlab("Daily Mean Normalized Air Temperature") +
+  ylab("Number of Total Bike Uses") +
+  geom_point(col="gray50") +
+  geom_smooth(method="gam", formula=y~s(x)) +
+  theme_bw()
+
+
+### Quantile trends
+
+ggplot(bike_share_daily, aes(x=temp, y=casual)) +
+  xlab("Daily Mean Normalized Temperature") +
+  ylab("Number of Casual Bike Uses") +
+  geom_point(col="gray50") +
+  stat_quantile(aes(color = ..quantile..), quantiles = c(0.05, 0.1, 0.25,
+  0.5, 0.75, 0.9, .95)) +
+  scale_color_gradient2(midpoint=0.5, low="steelblue", mid="brown", 
+  high="steelblue ") +
+  theme_bw()
+
+### Simple linear trends
+
+ggplot(bike_share_daily, aes(x=atemp, y=cnt)) +
+  xlab("Daily Mean Normalized Air Temperature") +
+  ylab("Number of Total Bike Uses") +
+  geom_point(col="gray50") +
+  geom_smooth(method="lm") +
+  theme_bw()
+
+
+### Segmented linear trends
+
+require(segmented)
+
+bike_segment = segmented(lm(cnt~atemp, data=bike_share_daily), ~atemp, psi=0.6)
+
+bike_segment$psi
+
+psi = bike_segment$psi[2]
+
+ggplot(bike_share_daily, aes(x=atemp, y=cnt, group = atemp > psi)) +
+  xlab("Daily Mean Normalized Air Temperature") +
+  ylab("Number of Total Bike Uses") +
+  geom_point(col="gray50") +
+  geom_smooth(method="lm") +
+  theme_bw()
+
+
+## The many flavors of regression
+
+# lm()
+# GLMs
+# glm()
+# glm.nb()
+# Proportional odds
+# polr()
+# Multinomial regression 
+# multinom()
+# Zero-inflated Poisson regression 
+# require (pscl)
+# zeroinfl() 
+# Zero-truncated Poisson regression 
+# require (gamlss.tr)
+# gamlss() 
+# GAM 
+# require(mgcv)
+# gam() 
+
+
+## Working with temporal data
+
+# http://cran.r-project.org/web/views/TimeSeries.html).
+
+# as.Date is usually best for dates 
+# as.POSIXct is usually best for times
+
+require(lubridate)
+
+india_natl_holidays = c("Jan 26, 2014", "Apr 13, 2014", "Aug 15, 2014", "Oct 02, 2014", "Dec 25, 2014")
+
+india_natl_holidays = as.Date(india_natl_holidays, format="%b %d, %Y")
+
+str(india_natl_holidays)
+
+Date[1:5], format: "2014-01-26" "2014-04-13" "2014-08-15" "2014-10-02" "2014-12-25"
+
+birthdays = c("20000101", "19991201", "19760704", "20140314")
+
+ymd(birthdays)
+
+age = ymd(today()) - ymd(birthdays)
+
+round(age/eyears(1), 1) # eyears is a lubridate function, not a typo!
+
+age = ymd(20141201) - ymd(birthdays)
+
+round(age/eyears(1), 0)
+
+?strptime 
+
+
+## Calculate a mean or correlation in circular time (clock time)
+
+require(psych)
+
+wake_time = c(7.0, 7.5, 6.5, 6.25, 7.0, 7.25, 7.25)
+
+sleep_time = c(23.5, 0.5, 23.75, 0.25, 23.25, 23.5, 0.75)
+
+wake_sleep = data.frame(wake_time, sleep_time)
+
+circadian.mean(wake_sleep$sleep_time)
+
+circadian.cor(wake_sleep)
+
+
+## Plotting time-series data
+
+library(eurostat)
+
+demo_fmonth = get_eurostat('demo_fmonth')
+
+demo_fmonth$date = as.Date(paste(substr(demo_fmonth$time, 1, 4), substr(demo_fmonth$month, 2, 3), "01", sep="-"))
+    
+library(dplyr)
+
+UK_births = filter(demo_fmonth, geo == "UK" & month != 'TOTAL' & month != 'UNK' & date >= '2003-01-01' & date <= '2012-12-01')
+
+UK_births = arrange(UK_births, date)
+
+UK_births_ts = ts(UK_births$values, start=c(2003, 1), frequency=12)
+
+plot(UK_births_ts, xlab="Date", ylab="Count", main="Monthly Births in the UK (2003-2012)", col="darkblue")
+
+require(ggplot2)
+
+ggplot(UK_births, aes(x=Date, y=Value)) +
+  geom_line(col="darkblue") +
+  ylab("Count") +
+  theme_minimal()
+
+
+## Detecting autocorrelation
+
+acf(UK_births_ts)
+
+
+## Plotting monthly patterns
+
+monthplot(UK_births_ts, main="Monthly Patterns in UK Births (2003-2012)", xlab="Month", ylab="Count", col="darkblue", lwd=2, lty.base=2, lwd.base=1, col.base="gray40")
+
+require(forecast)
+
+seasonplot(UK_births_ts, main="Seasonal Trends in UK Births (2003-2012)", col=rainbow(10), year.labels=TRUE, year.labels.left=TRUE, cex=0.7, cex.axis=0.8)
+
+
+## Decomposing time series into components
+
+### Additive decomposition
+
+plot(decompose(UK_births_ts), col="darkblue")
+
+
+### Decomposing when the variance changes
+
+data(AirPassengers)
+
+plot(AirPassengers)
+
+require(forecast)
+
+AirPassengers_lambda = BoxCox.lambda(AirPassengers)
+
+plot(BoxCox(AirPassengers, AirPassengers_lambda))
+
+plot(decompose(BoxCox(AirPassengers, AirPassengers_lambda)))
+
+
+## Finding cycles in the noise: Using spectral analysis to identify periodicity
+
+data(sunspot.year)
+
+sunny = TSA::periodogram(diff(sunspot.year))
+
+sunny_df = data.frame(freq = sunny$freq, spec = sunny$spec)
+
+sunny_df[which.max(sunny_df[,2]),]
+
+
+## Plotting survival curves
+
+require(survival)
+require(survminer)
+
+# Get data and add names of cancer type
+
+data(tongue, package = "KMsurv")
+
+tongue$tumor = ifelse(tongue$type==1, "Aneuploid",  "Diploid")
+
+# Create survival model
+tongue_survival = survfit(Surv(time = time, event = delta) ~ tumor, data = tongue)
+
+# Plot the curves and table
+ggsurvplot(tongue_survival, risk.table = TRUE, conf.int = TRUE)
+
+
+## Evaluating quality with control charts
+
+require(qcc)
+infections = c(6, 2, 5, 1, 3, 4, 2, 6, 3, 2, 4, 7, 1, 1, 4, 4, 1, 5, 2, 3, 5, 2, 3, 2, 4)
+
+patient_days = c(985, 778, 1010, 834, 750, 729, 1002, 639, 985, 578, 976, 540, 829, 723, 908, 1017, 1097, 1122, 1234, 1022, 1167, 1098, 1201, 1045, 1141)
+
+month_name = month.abb[c(1:12, 1:12, 1:1)]
+
+infection_control = qcc(infections, sizes=patient_days/1000, type="u", labels=month_name, axes.las=2, xlab="Month", ylab="", digits=2, title="Hospital Acquired Infections Rate per 1,000 Patient Days\n u-chart for Jan 2012 - Jan 2014")
+
+infection_control_qcc = data.frame(Month = month_name, infection_control$limits, Rate = (infections / patient_days)*1000)
+
+
+## Identifying possible breakpoints in a time series
+
+# http://www.eia.gov/beta/MER/index.cfm?tbl=T12.01#/?f=M&start=200101&end=201506&charted=0-1-13    
+
+require(strucchange)
+
+US_co2 = read.table("http://www.eia.gov/totalenergy/data/browser/csv.cfm?tbl=T12.01", sep=",", header=T)
+
+US_co2 = filter(US_co2, Column_Order == 14 & YYYYMM >= 200102 & substr(YYYYMM, 5, 7) != 13)
+
+US_co2_ts = ts(US_co2[,3], freq=12, start=c(2001,2))
+  
+# Obtain breakpoints estimate
+breakpoints(US_co2_ts ~ 1)
+
+# Plot the time series
+plot(US_co2_ts, main=expression(paste("Breakpoint in Monthly US"~CO[2]~Emissions)), ylab="Million Metric Tons", col="darkblue", lwd=1.5)
+
+# Plot the line at the optimal breakpoint
+lines(breakpoints(US_co2_ts ~ 1), col="darkgreen")
+
+# Plot a 90% confidence interval
+lines(confint(breakpoints(US_co2_ts ~ 1), level=0.90), col="darkgreen")
+
+# Add breakpoint location text
+text(2008.8, 555, "Jan 2009", cex=0.75, col="darkgreen", pos=4, font=3)
+
+
+## Exploring relationships between time series: cross-correlation
+
+# Load dplyr to subset the BLS data
+require(dplyr) 
+
+# Download CPI time series data from US BLS
+CPI_xport = read.table("http://download.bls.gov/pub/time.series/cu/cu.data.14.USTransportation", header=T, sep="\t", strip.white=T)
+
+# CPI for motor fuel, 2000-2014
+fuel = filter(CPI_xport, series_id == "CUUR0000SETB" & year >= 2000 & year <= 2014 & period != "M13")
+
+# CPI for airline fare, 2000-2014              
+fare = filter(CPI_xport, series_id == "CUUR0000SETG01" & year >= 2000 & year <= 2014 & period != "M13")
+
+# Create time series
+fuel_ts = ts(fuel$value, start=c(2000, 1), freq=12)
+
+fare_ts = ts(fare$value, start=c(2000, 1), freq=12)
+
+# CCF *requires* detrended data
+# Get detrended values of each time series
+
+fuel_detrend = na.omit(decompose(fuel_ts)$random)
+
+fare_detrend = na.omit(decompose(fare_ts)$random)
+
+acf(ts.union(fuel_detrend, fare_detrend))
+  
+# Calculate CCF with +/- 4 year span
+ccfvalues = ccf(fuel_detrend, fare_detrend, lag.max=48)
+
+ccfvalues
+
+# Function to find CCF
+Max_CCF = function(a, b) {
+  d = ccf(a, b, plot = FALSE, lag.max = length(a)-5)
+  cor = d$acf[,,1]
+  abscor = abs(d$acf[,,1])
+  lag = d$lag[,,1]
+  res = data.frame(cor,lag)
+  absres = data.frame(abscor,lag)
+  absres_max = res[which.max(absres$abscor),]
+  return(absres_max)
+}
+
+# Determine maximum CCF value
+Max_CCF(fuel_detrend, fare_detrend)
+
+
+## Basic forecasting
+
+require(forecast)
+require(dplyr)
+
+# Download data and load into R
+download.file("http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip", destfile = "Data/household_power_consumption.zip")
+
+unzip("Data/household_power_consumption.zip", exdir="Data")
+
+power = read.table("Data/household_power_consumption.txt", sep=";", header=T, na.strings=c("?",""), stringsAsFactors=FALSE)
+
+# Convert and create date objects
+power$Date = as.Date(power$Date, format="%d/%m/%Y")
+
+power$Month = format(power$Date,"%Y-%m")
+
+power$Month = as.Date(paste0(power$Month, "-01"))
+
+# Interpolate missing values and summarize monthly total power use
+power$Global_active_power_locf = na.locf(power$Global_active_power)
+
+power_group = group_by(power, Month)
+
+power_monthly = summarise(power_group, Total_Use_kWh = sum(Global_active_power_locf)/60)
+
+# Remove partial months from data frame
+power_monthly = power_monthly[2:47,]
+
+# Create a time series object of Total Usage
+total_use_ts = ts(power_monthly$Total_Use_kWh, start=c(2007,1), frequency=12)
+
+# Automatically obtain the forecast for the next 6 months
+total_use_fc = forecast(total_use_ts, h=6)
+
+# View the forecast model results
+summary(total_use_fc)
+
+plot(total_use_fc)
+
+
+##### End of File #####
