67390339820b883a65eb8098f8a3d8834a59671c
Rmadillo <Rmadillo@users.noreply.github.com>
2016-08-06 16:30:25 -0700

    minor code changes and formatting

--- manuscript/code/BIWR_Chapter_3_Code.R
+++ manuscript/code/BIWR_Chapter_3_Code.R
@@ -30,8 +30,7 @@ missing_data
 
 levels(customers$customer_gender)
 
-customers$customer_gender = ordered(customers$customer_gender, 
-
+customers$customer_gender = ordered(customers$customer_gender, c("Unknown", "F", "M"))
 
 ### Identifying unique values or duplicates
 
@@ -101,16 +100,18 @@ nursing_home_deficiencies$Date = clean(nursing_home_deficiencies$Date)
 nursing_home_deficiencies$Date = as.Date(nursing_home_deficiencies$Date, format="%B %d, %Y")
 
 # colnames(my_data)[column_number] = "new_column_name"
- 
 
 
 ### Missing data: Converting dummy NA values
 
 # This will work 
-my_data$my_variable = as.numeric(gsub("9999", NA, my_data$my_variable))
+# my_data$my_variable = as.numeric(gsub("9999", NA, my_data$my_variable))
 
-# But this is better
-my_data$my_variable[my_data$my_variable==9999] = NA 
+# But this is better, albeit less transparent
+# my_data$my_variable[my_data$my_variable==9999] = NA 
+
+# And this will do it for the entire data frame
+# my_data[my_data == 9999] = NA
 
 # This won't work
 mean(customers$customer_age)
@@ -143,13 +144,13 @@ signif(mean(customers$customer_age, na.rm=TRUE), 3)
 
 ### Concatenation
 
-my_data$date = as.Date(paste(mydata$yyyy_mm, "01", sep="-"))
+# my_data$date = as.Date(paste(mydata$yyyy_mm, "01", sep="-"))
 
-my_data$date = as.Date(paste0(mydata$yyyy_mm, "-01"))
+# my_data$date = as.Date(paste0(mydata$yyyy_mm, "-01"))
 
 plot(customers$customer_age, customers$customer_purchase, main=paste("Age vs Purchase Amount for", length(customers$customer_age), "Customers"))
 
-my_data$my_variable = as.numeric(paste(gsub(",", "", my_data$my_variable)))
+# my_data$my_variable = as.numeric(paste(gsub(",", "", my_data$my_variable)))
 
 
 ## Merging dataframes
@@ -171,25 +172,26 @@ customer_regions = merge(customer_locations, state_regions, by.x = "stabbr", by.
 
 customer_survey = merge(customer_regions, survey, by.x="customer_id", by.y="customer_id", all.x=TRUE)
 
-customer_survey = sqldf("SELECT
-                        customers.*
-                        , zipcodes.city
-                        , state_regions.state
-                        , state_regions.region
-                        , state_regions.division
-                        , survey.strongly_agree
-                        , survey.agree
-                        , survey.neutral
-                        , survey.disagree
-                        , survey.strongly_disagree
-                        FROM customers
-                        LEFT JOIN zipcodes
-                        ON customers.customer_zip = zipcodes.zip
-                        LEFT JOIN state_regions
-                        ON zipcodes.stabbr = state_regions.abb
-                        LEFT JOIN survey
-                        ON customers.customer_id = survey.customer_id"
-                        , stringsAsFactors=FALSE)
+customer_survey = sqldf("
+  SELECT
+    customers.*
+    , zipcodes.city
+    , state_regions.state
+    , state_regions.region
+    , state_regions.division
+    , survey.strongly_agree
+    , survey.agree
+    , survey.neutral
+    , survey.disagree
+    , survey.strongly_disagree
+  FROM customers
+  LEFT JOIN zipcodes
+    ON customers.customer_zip = zipcodes.zip
+  LEFT JOIN state_regions
+    ON zipcodes.stabbr = state_regions.abb
+  LEFT JOIN survey
+    ON customers.customer_id = survey.customer_id"
+  , stringsAsFactors=FALSE)
 
 
 # Left | left_join(A, B, by=c("A.key" ="B.key")) | merge(A, B, by.x="A.key", by.y="B.key", all.x=TRUE) | SELECT * FROM A LEFT JOIN B ON A.key = B.key 
@@ -319,20 +321,20 @@ head(surgery_outcomes_original, 4)
 
 ### Reshaping: Crossing variables with ~ and +
   
-head(dcast(surgery_outcomes_melted, ID ~ Test_type + Phase), 4)
+head(dcast(surgery_outcomes_melted, ID ~ Test_type + Phase, value.var='IQ_score'), 4)
   
-head(dcast(surgery_outcomes_melted, ID + Side ~ Test_type + Phase), 4)
+head(dcast(surgery_outcomes_melted, ID + Side ~ Test_type + Phase, value.var='IQ_score'), 4)
   
-head(dcast(surgery_outcomes_melted, ID + Phase ~ Test_type), 4)
+head(dcast(surgery_outcomes_melted, ID + Phase ~ Test_type, value.var='IQ_score'), 4)
 
 
 ### Summarizing while reshaping
 
-dcast(surgery_outcomes_melted, Phase ~ Test_type, mean, na.rm=T)
+dcast(surgery_outcomes_melted, Phase ~ Test_type, value.var='IQ_score', mean, na.rm=T)
   
-dcast(surgery_outcomes_melted, Side ~ Test_type + Phase, mean, na.rm=T)
+dcast(surgery_outcomes_melted, Side ~ Test_type + Phase, value.var='IQ_score', mean, na.rm=T)
   
-dcast(surgery_outcomes_melted, Test_type + Side + Phase ~ ., sd, na.rm=T)
+dcast(surgery_outcomes_melted, Test_type + Side + Phase ~ ., value.var='IQ_score', sd, na.rm=T)
 
 
 ## Piping with %>%: Stringing it together in dplyr
@@ -344,9 +346,9 @@ customer_500_piped = customer_survey %>%
     filter(customer_purchases >= 500) %>%   
     group_by(state) %>%  
     summarize(count_purchases = n(),  
-            mean_purchases = mean(customer_purchases, na.rm=T),  
-            sd_purchases = sd(customer_purchases, na.rm=T),  
-            sum_est = sum(est)) %>%  
+      mean_purchases = mean(customer_purchases, na.rm=T),  
+      sd_purchases = sd(customer_purchases, na.rm=T),  
+      sum_est = sum(est)) %>%  
     mutate(cv_purchases = round(sd_purchases / mean_purchases, 2))  
 
 
